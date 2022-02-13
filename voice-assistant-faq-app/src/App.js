47a6306236c9818747ed2e87dd1b8d70a77c5317
nikitasharma27 <nikita.sharma2710@gmail.com>
2022-02-13 20:42:15 +0400

    faq scroll implemented

--- voice-assistant-faq-app/src/App.js
+++ voice-assistant-faq-app/src/App.js
@@ -1,42 +1,46 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import React, { useState, useEffect, useRef } from 'react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { Navbar } from './Navbar';
+import { Faq } from './Faq';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { scroller } from 'react-scroll';
 
-function App() {
+export const App = () => {
+  const alanBtnInstance = useRef(null);
+  const [index, setIndex] = useState(null);
+  const [currentFaqId, setCurrentFaqId] = useState(null);
+  const [toggleColorFlag, setToggleColorFlag] = useState(false);
+
+  useEffect(() => {
+    if (!alanBtnInstance.current) {
+      alanBtnInstance.current = alanBtn({
+        key: 'a85acf818c7762c73470ffeb333fe9d52e956eca572e1d8b807a3e2338fdd0dc/stage',
+        onCommand: commandData => {
+          if (commandData.command === 'gotoFaq') {
+            scroller.scrollTo(`accordion-button-${commandData.faqId}`, {
+              duration: 800,
+              delay: 0,
+              smooth: 'easeInOutQuart',
+            });
+            setIndex(commandData.faqId - 1);
+            setCurrentFaqId(commandData.faqId);
+          } else if (commandData.command === 'toggleColorMode') {
+            setToggleColorFlag(flag => !flag);
+          }
+        },
+      });
+    }
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Navbar toggleColorFlag={toggleColorFlag} />
+      <Faq
+        index={index}
+        setIndex={setIndex}
+        currentFaqId={currentFaqId}
+        setCurrentFaqId={setCurrentFaqId}
+      />
     </ChakraProvider>
   );
-}
-
-export default App;
+};
--- voice-assistant-faq-app/src/Navbar.js
+++ voice-assistant-faq-app/src/Navbar.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import {Box, Flex, Heading, Spacer } from '@chakra-ui/react'
-import { ColorModeSwitcher } from './ColorModeSwitcher'
+import React from 'react';
+import { Box, Flex, Heading, Spacer } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
-export const Navbar = () => {
+export const Navbar = ({ toggleColorFlag }) => {
   return (
     <Flex align="center" boxShadow="base" p="2" mb="2">
-        <Box p="2">
-            <Heading size="lg">E-commerce Support</Heading>
-        </Box>
-        <Spacer />
-        <Box>
-            <ColorModeSwitcher></ColorModeSwitcher>
-        </Box>
+      <Box p="2">
+        <Heading size="lg">E-commerce Support</Heading>
+      </Box>
+      <Spacer />
+      <Box>
+        <ColorModeSwitcher toggleColorFlag={toggleColorFlag} />
+      </Box>
     </Flex>
-  )
-}
+  );
+};
